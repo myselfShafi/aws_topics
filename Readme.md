e613b63811d02887b0ee8e69e1fc024c52ebea47
shafi <116013717+myselfShafi@users.noreply.github.com>
2025-04-13 20:46:20 +0530

    s3 - put, list, delete

--- Readme.md
+++ Readme.md
@@ -1,3 +1,9 @@
-s3 - simple storage service
+s3 - simple storage service - multimedia asset storage cloud disk
+        - can have multiple(100/account) buckets - each bucket can have multiple objects/folders and subobjects/subfolders
+        - on s3, we can also static host our websites(put html/css files in bucket), and connect it to our custom website using cloudfront
+        - bucket names should be unique globally, irrespective of users or account.
         - buckets - public, private
         - private bucket requires a token and signature appended to url(as query params), calling it a presigned url
+        - presigned urls - we can acheive getObject, putObject
+
+        - event notifications - just like mongo-changestream, we can write lambda func to trigger after selected event like add, update, delete
--- s3/index.js
+++ s3/index.js
@@ -1,6 +1,7 @@
-import { GetObjectCommand, S3Client } from "@aws-sdk/client-s3";
+import { DeleteObjectCommand, GetObjectCommand, ListObjectsV2Command, PutObjectCommand, S3Client } from "@aws-sdk/client-s3";
 import { getSignedUrl } from "@aws-sdk/s3-request-presigner";
 
+// -------------------- config.js --------------------- //
 // create a user in --IAM > Users-- to get the below credentials
 const client = new S3Client({
   region: "aws_region",
@@ -10,24 +11,58 @@ const client = new S3Client({
   },
 });
 
+const bucketName = "aws_bucket_name";
+
+
+// these options can be specific for each use-case, i.e; in getObject, putObject
+let preSignOptions = {
+  expiresIn: 3600, // by default 900 seconds/15mins
+  hoistableHeaders: new Set[header_names], // Headers to include in the signed URL
+  unhoistableHeaders: new Set[header_names], // Headers to exclude from being signed
+  signableHeaders: new Set[header_names], // Headers allowed to be signed
+  unsignableHeaders: new Set[header_names], // Headers you want to exclude from signing
+  signingDate, // Set a specific date/time to use for signing. default: current time of request
+  signingRegion, // Override the AWS region used in signing
+  signingService, // Override the service used for signing (usually 's3')
+};
+
 async function getObjectURL(key) {
   const command = new GetObjectCommand({
     Bucket: "aws_bucket_name",
-    Key: key,
+    Key: key, // key name of the media file or object file in s3
   });
 
-  let options = {
-    expiresIn: 3600, // by default 900 seconds
-    hoistableHeaders: new Set[header_names], // Headers to include in the signed URL
-    unhoistableHeaders: new Set[header_names], // Headers to exclude from being signed
-    signableHeaders: new Set[header_names], // Headers allowed to be signed
-    unsignableHeaders: new Set[header_names], // Headers you want to exclude from signing
-    signingDate, // Set a specific date/time to use for signing. default: current time of request
-    signingRegion, // Override the AWS region used in signing
-    signingService, // Override the service used for signing (usually 's3')
-  };
+  // this generates a presignedurl for private bucket, security purposes
+  return await getSignedUrl(client, command, preSignOptions);
+};
+// await getObjectURL('bucket.jpeg');
+
+async function putObjectUrl(fileName, contentType) {
+  const command = new PutObjectCommand({
+    Bucket: bucketName,
+    Key: `uploads/media/${fileName}`, // path/location where you want to save file, in this case root folder is uploads
+    ContentType: contentType
+  });
 
   // this generates a presignedurl for private bucket, security purposes
-  const url = await getSignedUrl(client, command, options);
-  return url;
+  return await getSignedUrl(client, command, preSignOptions);
+};
+// await putObjectUrl(`img_${Date.now()}.jpeg`, 'image/jpeg');
+
+async function listObjects() {
+  const command = new ListObjectsV2Command({
+    Bucket: bucketName,
+    Key: `/`, // path/location 
+  });
+
+  return await client.send(command);
+};
+
+async function deleteObject() {
+  const command = new DeleteObjectCommand({
+    Bucket: bucketName,
+    Key: `/`, // path/location 
+  });
+
+  return await client.send(command);
 };
